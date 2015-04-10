768abb63f39c0611b44c215e3c330270cc6e946a
Stefan Iancu <a1b2ff@yahoo.com>
2015-04-11 00:15:13 +0300

    added missing files

--- config/routes.js
+++ config/routes.js
@@ -5,6 +5,8 @@ var paymentsController = require('../controllers/payments');
 module.exports = function (app, passport) {
 	app.get('/', indexController.index)
 	app.get('/about', aboutController.index)
-	app.get('/payments/create', paymentsController.index)
+	app.get('/payments/create', paymentsController.create)
+	app.post('/payments/post', paymentsController.post)
+	app.get('/payments/init', paymentsController.init)
 	
 }
--- controllers/payments.js
+++ controllers/payments.js
@@ -7,11 +7,60 @@ var mylogger = require('../node_modules_my/logger')
 
 var logger = mylogger()
 
-exports.index = function(req, res){
+exports.create = function(req, res){
+	var renderPayments = jade.compileFile('./views/paymentsCreate.jade', {pretty: true})
+ 	var htmlPayments  =  renderPayments()
+	res.send(htmlPayments)
+
+}
+
+exports.init = function(req,res){
+	res.send(config.get('testData'))
+}
+
+exports.post = function(req, res){
+	var message = {};
+    req.on('data', function (data) {
+    	
+        message = JSON.parse(data);
+        //console.log(message)
+        if (message.length > 1e6)
+            req.connection.destroy();
+
+    });
+    req.on('end', function () { 
+        //console.log(body['headers'])
+		 var options = {
+			 url: config.get('env.host') + '/payments',
+	    	 headers: {
+	        	"Authorization": "Basic " + message['headers']
+	    	},
+	    	body: message.body
+		}  	     
+		logger.info(options)
+		request.post(options, function (error, response, body){
+			 var parsedBody = null
+			  parsedBody = contentParse(body)
+			  if (!error && response.statusCode >= 200 && response.statusCode < 300) {
+			  	
+			    logger.info(response.statusCode)
+			    
+			  }
+			  else{
+			  	logger.error(response.statusCode)
+			  	
+			  }
+			  logger.info(body)
+			  res.send(JSON.stringify(parsedBody))
+		})
+
+	})
+}
+
+exports.get = function(req, res){
 
 	var renderPayments = jade.compileFile('./views/payments.jade', {pretty: true})
- 	
-	
+
 	logger.info('APIKEY:' + config.util.getEnv('APIKEY'))
 
 	var options = {
@@ -26,7 +75,7 @@ exports.index = function(req, res){
 	request.get(options, function (error, response, body) {				  
 		  
 		  var parsedBody = null
-		  if (!error && response.statusCode == 200) {
+		  if (!error && response.statusCode > 200 && response.code < 300) {
 		  	
 		    logger.info(body)
 		    parsedBody = contentParse(body)
